6f5c02c2857134d3cb32b7224939437f2b621725
Nurlan Məmmədov <nurlan.memmedov8818@gmail.com>
2024-07-04 21:43:17 +0400

    Test

---
+++ VCard/VCard/Helpers/Result.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using VCard.Models;
+
+namespace VCard.Helpers
+{
+    public class Result
+    {
+        public Name Name { get; set; }
+        public Location Location { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ VCard/VCard/Responses/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VCard.Helpers;
+
+namespace VCard.Responses
+{
+    public class ApiResponse
+    {
+        public List<Result> Results { get; set; }
+    }
+}
